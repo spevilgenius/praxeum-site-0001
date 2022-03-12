5741c9e71bce6f73f25b14ae472cd7f4e07136db
danwalker <dwalker@nexagen.onmicrosoft.us>
2022-03-12 14:49:34 +0000

    Initial Commit

---
+++ src/webparts/praxeumSite0001/PraxeumSite0001WebPart.ts
@@ -0,0 +1,61 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+    BaseClientSideWebPart,
+    IPropertyPaneConfiguration,
+    PropertyPaneTextField
+  } from '@microsoft/sp-webpart-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import * as strings from 'PraxeumSite0001WebPartStrings';
+
+// Importing Vue.js
+import Vue from 'vue';
+// Importing Vue.js SFC
+import PraxeumSite0001Component from './components/PraxeumSite0001.vue';
+
+export interface IPraxeumSite0001WebPartProps {
+  description: string;
+}
+
+export default class PraxeumSite0001WebPart extends BaseClientSideWebPart<IPraxeumSite0001WebPartProps> {
+
+  public render(): void {
+    const id: string = `wp-${this.instanceId}`;
+    this.domElement.innerHTML = `<div id="${id}"></div>`;
+
+    let el = new Vue({
+      el: `#${id}`,
+      render: h => h(PraxeumSite0001Component, {
+        props: {
+          description: this.properties.description
+        }
+      })
+    });
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/praxeumSite0001/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IPraxeumSite0001WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'PraxeumSite0001WebPartStrings' {
+  const strings: IPraxeumSite0001WebPartStrings;
+  export = strings;
+}
